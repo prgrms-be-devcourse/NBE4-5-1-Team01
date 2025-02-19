8f4a6aa0e07b68b8ee42e0e59af51928443488e0
jin214930 <jin214930@naver.com>
2025-02-19 16:46:09 +0900

    [fix] 백엔드 환경 설정 #1

---
+++ backend/src/test/java/com/team1/beanstore/BeanstoreApplicationTests.java
@@ -0,0 +1,14 @@
+package com.team1.beanstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("fdsfd");
+    }
+
+}
