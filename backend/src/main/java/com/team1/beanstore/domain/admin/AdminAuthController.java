91b3d64a1efcc887284b3192260924551d727aaf
asungkim <asungkim007@naver.com>
2025-02-20 15:01:02 +0900

    [feat] 로그인 완성 - Jwt 발급 및 검증 추가 #7

--- backend/src/main/java/com/team1/beanstore/domain/admin/AdminAuthController.java
+++ backend/src/main/java/com/team1/beanstore/domain/admin/AdminAuthController.java
@@ -20,6 +20,7 @@ import java.util.Map;
 public class AdminAuthController {
 
     private final PasswordEncoder passwordEncoder;
+    private final AuthTokenService authTokenService;
 
     @Value("${admin.password}")
     private String encodedPassword;
@@ -34,16 +35,19 @@ public class AdminAuthController {
         // 비밀번호가 맞으면 토큰 발급
         if (passwordEncoder.matches(password, encodedPassword)) {
             // JWT 발급
+            String jwtToken = authTokenService.genToken();
 
             return ResponseEntity.ok(
                     Map.of(
                             "status", 200,
-                            "msg", "로그인 성공"
+                            "msg", "로그인 성공",
+                            "token", jwtToken
                     )
             );
         }
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
                 .body(Map.of(
                         "status", 401,
                         "msg", "비밀번호가 틀렸습니다."
