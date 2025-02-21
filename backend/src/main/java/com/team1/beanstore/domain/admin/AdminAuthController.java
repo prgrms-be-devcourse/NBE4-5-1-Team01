64c842112da4fb99a5340b1f2b1bec0a042049d0
asungkim <asungkim007@naver.com>
2025-02-21 09:43:37 +0900

    [feat] RsData apply - 커스텀 응답 객체 적용 #12

--- backend/src/main/java/com/team1/beanstore/domain/admin/AdminAuthController.java
+++ backend/src/main/java/com/team1/beanstore/domain/admin/AdminAuthController.java
@@ -1,18 +1,15 @@
 package com.team1.beanstore.domain.admin;
 
+import com.team1.beanstore.global.dto.RsData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/GCcoffee/admin")
@@ -29,7 +26,7 @@ public class AdminAuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginReqBody body) {
+    public RsData<String> login(@Valid @RequestBody LoginReqBody body) {
         String password = body.password();
 
         // 비밀번호가 맞으면 토큰 발급
@@ -37,21 +34,17 @@ public class AdminAuthController {
             // JWT 발급
             String jwtToken = authTokenService.genToken();
 
-            return ResponseEntity.ok(
-                    Map.of(
-                            "status", 200,
-                            "msg", "로그인 성공",
-                            "token", jwtToken
-                    )
+            return new RsData<>(
+                    "200-1",
+                    "로그인 성공",
+                    jwtToken
             );
         }
 
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of(
-                        "status", 401,
-                        "msg", "비밀번호가 틀렸습니다."
-                ));
+        return new RsData<>(
+                "401-1",
+                "비밀번호가 틀렸습니다."
+        );
     }
 
 }
