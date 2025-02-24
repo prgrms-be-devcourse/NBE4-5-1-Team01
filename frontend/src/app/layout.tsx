ebc055c8d0845d5a07267b30d606a340687e4f1c
asungkim <asungkim007@naver.com>
2025-02-24 17:19:54 +0900

    [feat] 로그인 기능 구현 - 쿠키 인증 #49

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "./globals.css";
 import ClientLayout from "./ClientLayout";
+import { cookies } from "next/headers";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
 config.autoAddCss = false;
 
@@ -23,22 +25,23 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const myCookie = await cookies();
-  // const { isLogin, payload } = parseAccessToken(myCookie.get("accessToken"));
+  const myCookie = await cookies();
+  myCookie.get("accessToken");
+  const { isLogin, payload } = parseAccessToken(myCookie.get("accessToken"));
 
-  // const me = isLogin
-  //   ? {
-  //       id: payload.id,
-  //       nickname: payload.nickname,
-  //     }
-  //   : {
-  //       id: 0,
-  //       nickname: "",
-  //     };
+  const me = isLogin
+    ? {
+        id: payload.id,
+        role: payload.role,
+      }
+    : {
+        id: 0,
+        role: "",
+      };
 
   return (
     <ClientLayout
-      //me={me}
+      me={me}
       fontVariable={pretendard.variable}
       fontClassName={pretendard.className}
     >
@@ -47,22 +50,22 @@ export default async function RootLayout({
   );
 }
 
-// function parseAccessToken(accessToken: RequestCookie | undefined) {
-//   let isExpired = true;
-//   let payload = null;
+function parseAccessToken(accessToken: RequestCookie | undefined) {
+  let isExpired = true;
+  let payload = null;
 
-//   if (accessToken) {
-//     try {
-//       const tokenParts = accessToken.value.split(".");
-//       payload = JSON.parse(Buffer.from(tokenParts[1], "base64").toString());
-//       const expTimestamp = payload.exp * 1000; // exp는 초 단위이므로 밀리초로 변환
-//       isExpired = Date.now() > expTimestamp;
-//     } catch (e) {
-//       console.error("토큰 파싱 중 오류 발생:", e);
-//     }
-//   }
+  if (accessToken) {
+    try {
+      const tokenParts = accessToken.value.split(".");
+      payload = JSON.parse(Buffer.from(tokenParts[1], "base64").toString());
+      const expTimestamp = payload.exp * 1000; // exp는 초 단위이므로 밀리초로 변환
+      isExpired = Date.now() > expTimestamp;
+    } catch (e) {
+      console.error("토큰 파싱 중 오류 발생:", e);
+    }
+  }
 
-//   let isLogin = payload !== null;
+  let isLogin = payload !== null;
 
-//   return { isLogin, isExpired, payload };
-// }
+  return { isLogin, isExpired, payload };
+}
